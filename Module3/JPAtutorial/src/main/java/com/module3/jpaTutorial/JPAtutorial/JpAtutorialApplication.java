deaae686d550e16a3521e0bc6af9ce1986b843ec
harshchoudhary21 <harshchoudhary210j@gmail.com>
2026-01-10 13:35:24 +0530

    ADDED MODULE 3

---
+++ Module3/JPAtutorial/src/main/java/com/module3/jpaTutorial/JPAtutorial/JpAtutorialApplication.java
@@ -0,0 +1,13 @@
+package com.module3.jpaTutorial.JPAtutorial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpAtutorialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JpAtutorialApplication.class, args);
+	}
+
+}
---
+++ Module3/JPAtutorial/src/test/java/com/module3/jpaTutorial/JPAtutorial/JpAtutorialApplicationTests.java
@@ -0,0 +1,13 @@
+package com.module3.jpaTutorial.JPAtutorial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpAtutorialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
